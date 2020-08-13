b6ccf1983e0f23c19a59656ca5a56fcef4031c93
cgking <58586625+cgkings@users.noreply.github.com>
2020-08-13 10:03:57 +0800

    新增fsa功能

--- script/fsa.sh
+++ script/fsa.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-stty erase '^H'
-echo 正常的sa无输出，错误的才有，注意你的CPU负载！开始！
-read -p "输入你的sa保存目录！ : " safolder
-mkdir -p $safolder/invalid
-mkdir -p $safolder/ok
-read -p "输入你的remote！ : " remote
-read -p "输入你的团队盘id！ : " folderid
-sum_check=$(cd $safolder/invalid && ls -l | grep "^-" | wc -l)
-while [ $sum_check=[0] ];do
-find $safolder -type f -name "*.json" | xargs -I {} -n 1 -P 10 bash -c 'fclone lsd '$remote':{'$folderid'} --drive-service-account-file={} --drive-service-account-file-path=""  &> /dev/null || mv -f {} '$safolder'/invalid '
-sum_check=$(cd $safolder/invalid && ls -l | grep "^-" | wc -l)
-mv '$safolder'/*.json '$safolder'/ok
-    if [ $sum_check=[0] ];then
-    echo 恭喜你！你的sa检测ok！
-    exit
-    else
-    echo -e "已检测完毕,异常sa "$sum_check" 个，即将为你开启服务"
+source /root/fclone_shell_bot/myfc_config.ini
+# ★★★开启sa服务-已完成★★★
+open_sa_server() {
     sumsa=0
-        for saf_id in $(sort -u $safolder/invalid/*.json | grep "project_id" | awk '{print $2}' | tr -d ',"')
-        do
-        cd /root/AutoRclone && python3 gen_sa_accounts.py --enable-services $saf_id
-        sumsa=$((sumsa+1))
-        echo -e "已开启 第"$sumsa"个sa；共"$sum_check"个sa"
-        echo -e "done！！！"
-        done
-    fi
-mv $safolder/invalid/*.json $safolder
-sum_check=$(cd $safolder/invalid && ls -l | grep "^-" | wc -l)
-done
+    for saf_id in $(sort -u $safolder/invalid/*.json | grep "project_id" | awk '{print $2}' | tr -d ',"')
+    do
+    cd /root/AutoRclone && python3 gen_sa_accounts.py --enable-services $saf_id
+    sumsa=$((sumsa+1))
+    echo -e "已开启 第"$sumsa"个sa；共"$sum_check"个sa"
+    done
+}
 
-######safolder###                                                              ####remote#####            ###########33folderid###########          &> /dev/null
-#本地存放sa的目录##                                                          ##fclone config show 查看###   #######团队盘目录id############
-#######################请修改以上三个参数之后给脚本权限后执行脚本###############
+stty erase '^H'
+echo 读取myfc_config.ini,注意你的CPU负载！
+echo -e "你的remote:$fclone_name"
+echo -e "你的sa保存目录:$safolder"
+echo -e "你的sa检测目标文件夹id:$fsa_id"
+echo -e "开启sa服务所需的gen_sa_accounts.py文件所在目录：$pyfolder"
+echo -e "以上如需修改，请打开ini文件修改fclone_name\safolder\pyfolder\fsa_id数值"
+echo -e "检测NG.文件目录：$safolder/invalid"
+echo -e "检测ok文件将移至：/root/AutoRclone/$fclone_name，如需更改，请修改本脚本相应目录行即可"
+mkdir -p $safolder/invalid
+sa_sum=$(ls -l $safolder|grep "^-"| wc -l)
+echo -e "待检测sa $sa_sum 个，开始检测"
+find $safolder -type f -name "*.json" | xargs -I {} -n 1 -P 10 bash -c 'fclone lsd '$fclone_name':{1w-AqIaSU2zVG9N7ia_oNPB4qJrCOUQYG} --drive-service-account-file={} --drive-service-account-file-path=""  &> /dev/null || mv -f {'$fsa_id'} '$safolder'/invalid '
+xsa_sum=$(ls -l $safolder/invalid|grep "^-"| wc -l)
+sa_sum=$(ls -l $safolder|grep "^-"| wc -l)
+if [ x$xsa_sum = x0 ];then
+echo 恭喜你！你的sa[$sa_sum]全部检测ok！
+mv $safolder/*.json /root/AutoRclone/"$fclone_name"
+echo -e "检测ok sa已移至/root/AutoRclone/$fclone_name"
+elif [ x$sa_sum = x0 ];then
+echo 非常遗憾，$sa_sum 个sa,竟然没有一个是ok的，没关系，即将为你开启服务
+open_sa_server
+else
+mv $safolder/*.json /root/AutoRclone/"$fclone_name"
+echo -e "检测ok sa $sa_sum 个，已移至/root/AutoRclone/$fclone_name"
+echo -e "检测完毕，异常sa $xsa_sum 个,即将为你开启服务！！！"
+open_sa_server
+fi
+echo -e "done！！！"
