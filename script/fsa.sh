7a682d2647c254dc307f4e5694621e334db1ba0a
cgking <58586625+cgkings@users.noreply.github.com>
2020-08-13 10:38:59 +0800

    Update fsa.sh

--- script/fsa.sh
+++ script/fsa.sh
@@ -21,8 +21,8 @@ echo -e "以上如需修改，请打开ini文件修改fclone_name\safolder\pyfol
 echo -e "检测NG.文件目录：$safolder/invalid"
 echo -e "检测ok文件将移至：/root/AutoRclone/$fclone_name，如需更改，请修改本脚本相应目录行即可"
 mkdir -p $safolder/invalid
-sa_sum=$(ls -l $safolder|grep "^-"| wc -l)
-echo -e "待检测sa $sa_sum 个，开始检测"
+sa1_sum=$(ls -l $safolder|grep "^-"| wc -l)
+echo -e "待检测sa $sa1_sum 个，开始检测"
 find $safolder -type f -name "*.json" | xargs -I {} -n 1 -P 10 bash -c 'fclone lsd '$fclone_name':{'$fsa_id'} --drive-service-account-file={} --drive-service-account-file-path=""  &> /dev/null || mv {} '$safolder'/invalid '
 xsa_sum=$(ls -l $safolder/invalid|grep "^-"| wc -l)
 sa_sum=$(ls -l $safolder|grep "^-"| wc -l)
@@ -32,7 +32,7 @@ echo 恭喜你！你的sa[$sa_sum]全部检测ok！
 mv -f $safolder/*.json /root/AutoRclone/"$fclone_name"
 echo -e "检测ok sa已移至/root/AutoRclone/$fclone_name,现$fclone_name文件夹共有$ok_sum个sa"
 elif [ x$sa_sum = x0 ];then
-echo 非常遗憾，$sa_sum 个sa,竟然没有一个是ok的，没关系，即将为你开启服务
+echo 非常遗憾，$sa1_sum 个sa,全军覆没了，没关系，即将为你开启服务
 open_sa_server
 else
 mv -f $safolder/*.json /root/AutoRclone/"$fclone_name"
