e5c785c7a536e1a704a3eac713978d1a0c5b3184
cgking <58586625+cgkings@users.noreply.github.com>
2020-09-04 19:16:23 +0800

    Update test.sh

--- script/bak/test/test.sh
+++ script/bak/test/test.sh
@@ -2,4 +2,6 @@
 for x in /root/AutoRclone/test/*.json ; 
 do
 echo $x
+proj_name=$(cat /root/AutoRclone/test/66edfbdabf44edb7bbe0bb9a35b2126eedc70ded.json | grep "project_id" | awk '{print $2}'| tr -d ',"')
+echo $proj_name
 done
