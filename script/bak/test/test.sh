480d760c147df20488f6c2d9833d89950d81f5bf
cgking <58586625+cgkings@users.noreply.github.com>
2020-09-04 19:51:09 +0800

    Update test.sh

--- script/bak/test/test.sh
+++ script/bak/test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-read -p "请输入需要改名sa所在目录路径" sa_path
-read -p "请输入想要分割项目名和sa名的符号" sa_name_mark
-for x in $sa_path/*.json ; 
+read -p "请输入需要改名sa所在目录路径:" sa_path
+read -p "请输入想要分割项目名和sa名的符号:" sa_name_mark
+for x in $sa_path/*.json ;
 do
 proj_name=$(cat $x | grep "project_id" | awk '{print $2}'| tr -d ',"')
 sa_name=$(cat $x | grep "client_email" | awk '{print $2}'| tr -d ',"' | cut -d@ -f1)
