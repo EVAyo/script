81def4673ef7c7ff58d7a8990f4867d425e1947e
cgking <58586625+cgkings@users.noreply.github.com>
2020-09-04 19:47:53 +0800

    Update test.sh

--- script/bak/test/test.sh
+++ script/bak/test/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-for x in /root/AutoRclone/test/*.json ; 
+read -p "请输入需要改名sa所在目录路径" sa_path
+read -p "请输入想要分割项目名和sa名的符号" sa_name_mark
+for x in $sa_path/*.json ; 
 do
 proj_name=$(cat $x | grep "project_id" | awk '{print $2}'| tr -d ',"')
 sa_name=$(cat $x | grep "client_email" | awk '{print $2}'| tr -d ',"' | cut -d@ -f1)
-mv $x /root/AutoRclone/test/"$proj_name"_"$sa_name".json
+mv $x "$sa_path"/"$proj_name""$sa_name_mark""$sa_name".json
 done
