f3bdeb0a9875cc21d72125a02f3efd64c618e448
cgking <58586625+cgkings@users.noreply.github.com>
2020-09-04 19:38:41 +0800

    Update test.sh

--- script/bak/test/test.sh
+++ script/bak/test/test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 for x in /root/AutoRclone/test/*.json ; 
 do
-echo $x
 proj_name=$(cat $x | grep "project_id" | awk '{print $2}'| tr -d ',"')
-echo $proj_name
 sa_name=$(cat $x | grep "client_email" | awk '{print $2}'| tr -d ',"' | cut -d@ -f1)
-echo $sa_name
+mv $x /root/AutoRclone/test/"$proj_name"_"$sa_name".json
 done
