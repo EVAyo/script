a17c661d93dcf3bc1be25b1bf9bf3e068bee86f2
cgking <58586625+cgkings@users.noreply.github.com>
2020-09-11 14:03:15 +0800

    Update fpcopy.sh

--- script/fpcopy.sh
+++ script/fpcopy.sh
@@ -10,11 +10,11 @@
 #=============================================================
 
 source /root/fclone_shell_bot/myfc_config.ini
-: > /root/fclone_shell_bot/log/fpcopy1.log
-: > /root/fclone_shell_bot/log/fpcopy2.log
+: > /root/fclone_shell_bot/log/fpcopy.log
+: > /root/fclone_shell_bot/log/fpsync.log
 : > /root/fclone_shell_bot/log/fdedupe.log
+: > /root/fclone_shell_bot/log/fcleanup.log
 
-clear
 read -p "【点对点模式】请输入from ID==>" link1
 link1=${link1#*id=};link1=${link1#*folders/};link1=${link1#*d/};link1=${link1%?usp*}
 if [ -z "$link1" ] ; then
@@ -26,12 +26,15 @@ if [ -z "$link2" ] ; then
 echo "不允许输入为空" && exit ;
 fi
 echo -e "▣▣▣▣▣▣执行转存▣▣▣▣▣▣"
-fclone copy "$fclone_name":{$link1} "$fclone_name":{$link2} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers="$fp_chercker" --transfers="$fp_transfer" --drive-pacer-min-sleep="$fp_min_sleep"ms --drive-pacer-burst="$fp_BURST" --min-size "$fp_min_size"M --check-first --ignore-existing --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fpcopy1.log
+fclone copy "$fclone_name":{$link1} "$fclone_name":{$link2} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers="$fp_chercker" --transfers="$fp_transfer" --drive-pacer-min-sleep="$fp_min_sleep"ms --drive-pacer-burst="$fp_BURST" --min-size "$fp_min_size"M --check-first --ignore-existing --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fpcopy.log
 echo -e "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
-echo -e "▣▣▣▣▣▣执行补缺▣▣▣▣▣▣"
-fclone copy "$fclone_name":{$link1} "$fclone_name":{$link2} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers="$fp_chercker" --transfers="$fp_transfer" --drive-pacer-min-sleep="$fp_min_sleep"ms --drive-pacer-burst="$fp_BURST" --min-size "$fp_min_size"M --check-first --ignore-existing --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fpcopy2.log
+echo -e "▣▣▣▣▣▣执行同步▣▣▣▣▣▣"
+fclone sync "$fclone_name":{$link1} "$fclone_name":{$link2} --drive-server-side-across-configs --drive-use-trash=false --stats=1s --stats-one-line -P --checkers="$fb_chercker" --transfers="$fb_transfer" --drive-pacer-min-sleep="$fb_min_sleep"ms --drive-pacer-burst="$fb_BURST" --min-size "$fb_min_size"M --check-first --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fpsync.log
 echo -e "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  补缺完毕"
-echo -e "▣▣▣▣▣▣执行查重▣▣▣▣▣▣"
-fclone dedupe newest "$fclone_name":{$link2} --fast-list --size-only --drive-use-trash=false --no-traverse --checkers="$fs_chercker" --transfers="$fs_transfer" -q --log-level=ERROR --log-file=/root/fclone_shell_bot/log/fdedupe.log
-echo -e "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
+echo -e "▣▣▣▣▣▣正在执行查重▣▣▣▣▣▣"
+fclone dedupe smallest "$fclone_name":{$link2} --drive-server-side-across-configs --drive-use-trash=false --checkers="$fs_chercker" --transfers="$fs_transfer" --drive-pacer-min-sleep="$fs_min_sleep"ms --drive-pacer-burst="$fs_BURST" --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fdedupe.log --check-first
+echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
+echo -e "▣▣▣▣▣▣正在执行删空▣▣▣▣▣▣"
+fclone rmdirs "$fclone_name":{$link2} --drive-use-trash=false -vvP --checkers="$fs_chercker" --transfers="$fs_transfer" --drive-pacer-min-sleep="$fs_min_sleep"ms --drive-pacer-burst="$fs_BURST" --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fcleanup.log --check-first
+echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  删空完毕"
 exit
