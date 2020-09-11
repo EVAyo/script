0b4c3f98ad4798b5c6633c4619346815330139d9
cgking <58586625+cgkings@users.noreply.github.com>
2020-09-11 14:54:36 +0800

    增加移动阵亡sa

--- script/fpcopy.sh
+++ script/fpcopy.sh
@@ -3,7 +3,7 @@
 # https://github.com/cgkings/fclone_shell_bot
 # File Name: fpcopy.sh
 # Author: cgking
-# Created Time : 2020.7.8
+# Created Time : 2020.9.11
 # Description:点对点转存
 # System Required: Debian/Ubuntu
 # Version: final
@@ -26,15 +26,33 @@ if [ -z "$link2" ] ; then
 echo "不允许输入为空" && exit ;
 fi
 echo -e "▣▣▣▣▣▣执行转存▣▣▣▣▣▣"
-fclone copy "$fclone_name":{$link1} "$fclone_name":{$link2} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers="$fp_chercker" --transfers="$fp_transfer" --drive-pacer-min-sleep="$fp_min_sleep"ms --drive-pacer-burst="$fp_BURST" --min-size "$fp_min_size"M --check-first --ignore-existing --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fpcopy.log
+fclone copy "$fclone_name1":{$link1} "$fclone_name1":{$link2} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers="$fp_chercker" --transfers="$fp_transfer" --drive-pacer-min-sleep="$fp_min_sleep"ms --drive-pacer-burst="$fp_BURST" --min-size "$fp_min_size"M --check-first --ignore-existing --log-level="$fp_log_level" --log-file=/root/fclone_shell_bot/log/fpcopy.log
 echo -e "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
 echo -e "▣▣▣▣▣▣执行同步▣▣▣▣▣▣"
-fclone sync "$fclone_name":{$link1} "$fclone_name":{$link2} --drive-server-side-across-configs --drive-use-trash=false --stats=1s --stats-one-line -P --checkers="$fb_chercker" --transfers="$fb_transfer" --drive-pacer-min-sleep="$fb_min_sleep"ms --drive-pacer-burst="$fb_BURST" --min-size "$fb_min_size"M --check-first --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fpsync.log
+fclone sync "$fclone_name1":{$link1} "$fclone_name1":{$link2} --drive-server-side-across-configs --drive-use-trash=false --stats=1s --stats-one-line -P --checkers="$fb_chercker" --transfers="$fb_transfer" --drive-pacer-min-sleep="$fb_min_sleep"ms --drive-pacer-burst="$fb_BURST" --min-size "$fb_min_size"M --check-first --log-level="$fp_log_level" --log-file=/root/fclone_shell_bot/log/fpsync.log
 echo -e "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  补缺完毕"
 echo -e "▣▣▣▣▣▣正在执行查重▣▣▣▣▣▣"
-fclone dedupe smallest "$fclone_name":{$link2} --drive-server-side-across-configs --drive-use-trash=false --checkers="$fs_chercker" --transfers="$fs_transfer" --drive-pacer-min-sleep="$fs_min_sleep"ms --drive-pacer-burst="$fs_BURST" --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fdedupe.log --check-first
+fclone dedupe smallest "$fclone_name1":{$link2} --drive-server-side-across-configs --drive-use-trash=false --checkers="$fs_chercker" --transfers="$fs_transfer" --drive-pacer-min-sleep="$fs_min_sleep"ms --drive-pacer-burst="$fs_BURST" --log-level="$fp_log_level" --log-file=/root/fclone_shell_bot/log/fdedupe.log --check-first
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
 echo -e "▣▣▣▣▣▣正在执行删空▣▣▣▣▣▣"
-fclone rmdirs "$fclone_name":{$link2} --drive-use-trash=false -vvP --checkers="$fs_chercker" --transfers="$fs_transfer" --drive-pacer-min-sleep="$fs_min_sleep"ms --drive-pacer-burst="$fs_BURST" --log-level=DEBUG --log-file=/root/fclone_shell_bot/log/fcleanup.log --check-first
+fclone rmdirs "$fclone_name1":{$link2} --drive-use-trash=false -vvP --checkers="$fs_chercker" --transfers="$fs_transfer" --drive-pacer-min-sleep="$fs_min_sleep"ms --drive-pacer-burst="$fs_BURST" --log-level="$fp_log_level" --log-file=/root/fclone_shell_bot/log/fcleanup.log --check-first
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  删空完毕"
+cat /root/fclone_shell_bot/log/fpcopy.log | grep "Changing Service Account File from" | awk '{print $10}' >> /root/fclone_shell_bot/log/invalid_list.log
+cat /root/fclone_shell_bot/log/fpsync.log | grep "Changing Service Account File from" | awk '{print $10}' >> /root/fclone_shell_bot/log/invalid_list.log
+cat /root/fclone_shell_bot/log/fdedupe.log | grep "Changing Service Account File from" | awk '{print $10}' >> /root/fclone_shell_bot/log/invalid_list.log
+cat /root/fclone_shell_bot/log/fcleanup.log | grep "Changing Service Account File from" | awk '{print $10}' >> /root/fclone_shell_bot/log/invalid_list.log
+if [ -s /root/fclone_shell_bot/log/invalid_list.log ] ; then
+sa_invalid_num=$(sed -n '$=' /root/fclone_shell_bot/log/invalid_list.log)
+mkdir -p /root/AutoRclone/"$fclone_name1"/invalid
+echo -e "本次转存阵亡"$sa_invalid_num"个sa，即将把阵亡sa转入/root/AutoRclone/"$fclone_name1"/invalid"
+for sa_invalid in $(cat /root/fclone_shell_bot/log/invalid_list.log)
+do
+mv -f "$sa_invalid" /root/AutoRclone/"$fclone_name1"/invalid
+done
+invalid_sum=$(ls -l /root/AutoRclone/"$fclone_name1"/invalid|grep "^-"| wc -l)
+echo -e "已转移"$invalid_sum"个sa，过24h自动转回"
+else
+echo -e "转存太轻松，没有阵亡的sa"
+exit
+fi
 exit
