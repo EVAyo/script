3a6da864c9fc68c3499fd7c3fcbd5f2d11753076
“TTfive” <asecondfive@gmail.com>
2021-01-07 00:25:15 +0800

    自动预约

--- jd_seckill/jd_spider_requests.py
+++ jd_seckill/jd_spider_requests.py
@@ -435,7 +435,7 @@ class JdSeckill(object):
         self._seckill()
 
     @check_login_and_jdtdufp
-    def seckill_by_proc_pool(self, work_count=1):
+    def seckill_by_proc_pool(self, work_count=5):
         """
         多进程进行抢购
         work_count：进程数量
