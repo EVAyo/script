67f19d2cbfda130d9041fccf5d25f5724a581f07
Dellear <358009775@qq.com>
2021-03-20 00:03:42 +0800

    修复PS1变量

--- start_panel.sh
+++ start_panel.sh
@@ -4,22 +4,24 @@
 ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
 PanelDir=${ShellDir}/panel
 
+export PS1="\u@\h:\w $"
+
 ## 启动ttyd
 case "$(uname -m)" in
 x86_64)
     echo -e "CPU架构:x86_64\n"
     cp -f ${PanelDir}/ttyd/ttyd.x86_64 /usr/local/bin/ttyd && chmod +x /usr/local/bin/ttyd
-    nohup ttyd -t fontSize=14 -t disableLeaveAlert=true -t rendererType=webgl sh >/dev/null 2>&1 &
+    nohup ttyd -t fontSize=14 -t disableLeaveAlert=true -t rendererType=webgl bash >/dev/null 2>&1 &
     ;;
 aarch64)
     echo -e "CPU架构:aarch64\n"
     cp -f ${PanelDir}/ttyd/ttyd.aarch64 /usr/local/bin/ttyd && chmod +x /usr/local/bin/ttyd
-    nohup ttyd -t fontSize=14 -t disableLeaveAlert=true -t rendererType=webgl sh >/dev/null 2>&1 &
+    nohup ttyd -t fontSize=14 -t disableLeaveAlert=true -t rendererType=webgl bash >/dev/null 2>&1 &
     ;;
 armv7l)
     echo -e "CPU架构:armv7l\n"
     cp -f ${PanelDir}/ttyd/ttyd.arm /usr/local/bin/ttyd && chmod +x /usr/local/bin/ttyd
-    nohup ttyd -t fontSize=14 -t disableLeaveAlert=true -t rendererType=webgl sh >/dev/null 2>&1 &
+    nohup ttyd -t fontSize=14 -t disableLeaveAlert=true -t rendererType=webgl bash >/dev/null 2>&1 &
     ;;
 *)
     echo -e "CPU架构暂不支持，无法正常使用终端面板！\n"
