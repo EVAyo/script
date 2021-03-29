daa13c69015d1d9c6c4a04b473d658f54942189a
anonymous <anonymous@gmail.com>
2021-03-29 23:36:54 +0800

    fix: 以-开头的pin无法被printf的bug

--- jcode.sh
+++ jcode.sh
@@ -21,7 +21,7 @@ gen_pt_pin_array () {
         tmp2=${!tmp1}
         i=$(($user_num - 1))
         pt_pin_temp=$(echo $tmp2 | perl -pe "{s|.*pt_pin=([^; ]+)(?=;?).*|\1|; s|%|\\\x|g}")
-        pt_pin[i]=$(printf $pt_pin_temp)
+        [[ $pt_pin_temp == *\\x* ]] && pt_pin[i]=$(printf $pt_pin_temp) || pt_pin[i]=$pt_pin_temp
     done
 }
 
