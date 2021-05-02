3557e3b54cf6712f35520d844db3518437c6e49c
anonymous <anonymous@hotmail.com>
2021-05-02 19:57:26 +0800

    add ignore watch file

--- jbot/ecosystem.config.js
+++ jbot/ecosystem.config.js
@@ -11,7 +11,10 @@ module.exports = {
             "jbot/__pycache__/*",
             "jbot/bot/__pycache__/*",
             "jbot/diy/__pycache__/*",
-            "jbot/*.log"
+            "jbot/*.log",
+            "jbot/*/*.log",
+            "jbot/requirements.txt",
+            "jbot/ecosystem.config.js"
         ],
         watch_delay: 15000,
         interpreter: ""
