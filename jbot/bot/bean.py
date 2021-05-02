5b8ddbc9a24e88c8ed6654feb2dbbc69bb7c8f1b
SuMaiKaDe <hbtsss@163.com>
2021-05-02 15:38:34 +0800

    fix
    再次调整图片大小

--- jbot/bot/bean.py
+++ jbot/bot/bean.py
@@ -56,7 +56,7 @@ def creat_bean_count(count):
             columns.append(line.split(',')[int(count)])
         tb.add_column(key, columns)
     length = 172 + 100 * 3
-    im = Image.new("RGB", (length, 240), (244, 244, 244))
+    im = Image.new("RGB", (length, 260), (244, 244, 244))
     dr = ImageDraw.Draw(im)
     font = ImageFont.truetype(_font, 18)
     dr.text((10, 5), str(tb), font=font, fill="#000000")
@@ -83,7 +83,7 @@ def creat_bean_counts(csv_file):
                 row.append(0)
         tb.add_row(row)
     length = 172 + 100 * num
-    im = Image.new("RGB", (length, 360), (244, 244, 244))
+    im = Image.new("RGB", (length, 4000), (244, 244, 244))
     dr = ImageDraw.Draw(im)
     font = ImageFont.truetype(_font, 18)
     dr.text((10, 5), str(tb), font=font, fill="#000000")
