49c1e8145aca0afc1ba9792e8f8e06cfc70d1f9d
anonymous <anonymous@hotmail.com>
2021-05-05 21:30:35 +0800

    fix

--- jup.sh
+++ jup.sh
@@ -550,7 +550,7 @@ fix_crontab () {
 
 update_notify () {
     if [[ $JD_DIR ]]; then
-        if [ -f /usr/local/bin/docker-entrypoint.sh ]; then
+        if [ -f /usr/local/bin/docker-entrypoint.sh ] && [ ! -d /etc/cont-init.d ] && [ ! -d /etc/services.d ]; then
             notify "镜像更新通知" "Docker镜像的启动方式已从docker-entrypoint调整为s6-overlay，请更新镜像，旧的镜像即将无法使用。" &>/dev/null
         fi
     fi
