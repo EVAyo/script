c227907dca1e32e5a89f7b6301c2b23517caede5
anonymous <anonymous@hotmail.com>
2021-05-12 14:41:42 +0800

    gen_pt_pin_array调整为共享函数

--- jcode.sh
+++ jcode.sh
@@ -12,18 +12,6 @@ import_config_and_check
 count_user_sum
 detect_termux
 
-## 生成pt_pin清单
-gen_pt_pin_array () {
-    local tmp1 tmp2 i pt_pin_temp
-    for ((user_num=1; user_num<=$user_sum; user_num++)); do
-        tmp1=Cookie$user_num
-        tmp2=${!tmp1}
-        i=$(($user_num - 1))
-        pt_pin_temp=$(echo $tmp2 | perl -pe "{s|.*pt_pin=([^; ]+)(?=;?).*|\1|; s|%|\\\x|g}")
-        [[ $pt_pin_temp == *\\x* ]] && pt_pin[i]=$(printf $pt_pin_temp) || pt_pin[i]=$pt_pin_temp
-    done
-}
-
 ## 导出互助码的通用程序，$1：去掉后缀的脚本名称，$2：config.sh中的后缀，$3：活动中文名称
 export_codes_sub () {
     local task_name=$1
