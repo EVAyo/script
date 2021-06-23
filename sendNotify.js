591dcc8e0345817233f7cda07197b9866650a604
ekoMax <ekoMax@gmail.com>
2021-06-23 08:13:01 +0800

    update

--- sendNotify.js
+++ sendNotify.js
@@ -148,9 +148,9 @@ if (process.env.PUSH_PLUS_USER) {
  * @param author 作者仓库等信息  例：`本脚本免费使用 By：xxxx`
  * @returns {Promise<unknown>}
  */
-async function sendNotify(text, desp, params = {}, author = '\n\n仅供用于学习 https://t.me/jdfruit') {
+async function sendNotify(text, desp, params = {}, author = '\n\n通知频道 https://t.me/sheeplost') {
   //提供6种通知
-  //desp += author;//增加作者信息，防止被贩卖等
+  desp += author;//增加作者信息，防止被贩卖等
   await Promise.all([
     serverNotify(text, desp),//微信server酱
     pushPlusNotify(text, desp)//pushplus(推送加)
