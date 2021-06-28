86bffdd4c6424048b20654ff9a48a15b90993667
ekoMax <ekoMax@gmail.com>
2021-06-28 13:46:50 +0800

    del

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,8 +4,6 @@
 30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
 ##############短期活动##############
-#京享值PK
-45 0,6,13,19,21 * * * node /scripts/jd_ddopk.js >> /scripts/logs/jd_ddopk.log 2>&1
 
 #女装盲盒 活动时间：2021-05-24到2021-06-22
 35 1,22 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
