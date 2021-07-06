b07e6faee5c0780799796ff87ff0039d438c1bee
ekoMax <ekoMax@gmail.com>
2021-07-06 12:19:44 +0800

    fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -26,6 +26,8 @@
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
+# 图形验证签到
+37 0,9 * * * node /scripts/jd_sign.js >> /scripts/logs/jd_sign.log 2>&1
 # 东东超市兑换奖品
 0,30 0 * * * node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
 # 摇京豆
