265907cd85bd9145dac4b7ed5312976f88f6f369
ekoMax <ekoMax@gmail.com>
2021-07-15 18:24:05 +0800

    fix

--- jd_sign.js
+++ jd_sign.js
@@ -1,13 +1,18 @@
 /* 
-
+cron 14 10 * * * jd_sign_graphics.js
 只支持nodejs环境
-14 10 * * *
+需要安装依赖 
+npm i png-js 或者 npm i png-js -S
+
+如果 read ECONNRESET 错误 可以试试
+环境变量 JOY_HOST
+修改域名 https://jdjoy.jd.com 可以改成ip https://49.7.27.236
 */
+
+const validator = require('./JDJRValidator_Pure.js');
+const Faker=require('./sign_graphics_validate.js') 
+
 const $ = new Env('京东签到图形验证');
-const validator = require('./utils/JDJRValidator_Pure.js');
-const Faker=require('./utils/jd_sign_validate.js') 
-$.get = validator.injectToRequest($.get.bind($), 'channelSign')
-$.post = validator.injectToRequest($.post.bind($), 'channelSign')
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
@@ -27,6 +32,10 @@ let UA = ""
 let signFlag = false
 let successNum = 0
 let errorNum = 0
+let JD_API_HOST = 'https://jdjoy.jd.com'
+if(process.env.JOY_HOST){
+  JD_API_HOST = process.env.JOY_HOST
+}
 
 const turnTableId = [
   { "name": "京东商城-内衣", "id": 1071, "url": "https://prodev.m.jd.com/mall/active/4PgpL1xqPSW1sVXCJ3xopDbB1f69/index.html" },
@@ -38,6 +47,8 @@ const turnTableId = [
   { "name": "京东商城-数码", "id": 347, "url": "https://prodev.m.jd.com/mall/active/4SWjnZSCTHPYjE5T7j35rxxuMTb6/index.html" },
   { "name": "京东超市", "id": 1204, "url": "https://pro.m.jd.com/mall/active/QPwDgLSops2bcsYqQ57hENGrjgj/index.html" },
 ]
+$.get = validator.injectToRequest($.get.bind($), 'channelSign')
+$.post = validator.injectToRequest($.post.bind($), 'channelSign')
 
 !(async () => {
   if (!cookiesArr[0]) {
@@ -78,7 +89,7 @@ async function showMsg() {
   if ($.isNode() && message) await notify.sendNotify(`${$.name}`, `【签到数量】:  ${turnTableId.length}个\n` + subTitle + message);
 }
 async function signRun() {
-  UA = $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;10.0.2;14.3;network/wifi;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
+  UA = $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
   for (let i in turnTableId) {
     signFlag = false
     await Login(i)
@@ -134,7 +145,7 @@ function Login(i) {
       try {
         if (err) {
           console.log(`\n${turnTableId[i].name} 登录: API查询请求失败 ‼️‼️`)
-          throw new Error(err);
+          console.log(`${JSON.stringify(err)}`)
         } else {
           if (data) {
             // console.log(data)
@@ -217,7 +228,7 @@ function getEid(arr) {
 }
 
 function taskUrl(turnTableId) {
-  const url = `https://jdjoy.jd.com/api/turncard/channel/detail?turnTableId=${turnTableId}&invokeKey=qRKHmL4sna8ZOP9F`
+  const url = `${JD_API_HOST}/api/turncard/channel/detail?turnTableId=${turnTableId}&invokeKey=qRKHmL4sna8ZOP9F`
   return {
     url,
     headers: {
@@ -226,6 +237,7 @@ function taskUrl(turnTableId) {
       "Accept-Language": "zh-cn",
       "Connection": "keep-alive",
       'Cookie': cookie,
+      'Host': `jdjoy.jd.com`,
       "Origin": "https://prodev.m.jd.com",
       "Referer": "https://prodev.m.jd.com/",
       "User-Agent": UA,
@@ -234,7 +246,7 @@ function taskUrl(turnTableId) {
 }
 
 function tasPostkUrl(turnTableId) {
-  const url = `https://jdjoy.jd.com/api/turncard/channel/sign?turnTableId=${turnTableId}&fp=${fp}&eid=${eid}&invokeKey=qRKHmL4sna8ZOP9F`
+  const url = `${JD_API_HOST}/api/turncard/channel/sign?turnTableId=${turnTableId}&fp=${fp}&eid=${eid}&invokeKey=qRKHmL4sna8ZOP9F`
   return {
     url,
     headers: {
@@ -244,6 +256,7 @@ function tasPostkUrl(turnTableId) {
       "Connection": "keep-alive",
       "Content-Type": "application/x-www-form-urlencoded",
       'Cookie': cookie,
+      'Host': `jdjoy.jd.com`,
       "Origin": "https://prodev.m.jd.com",
       "Referer": "https://prodev.m.jd.com/",
       "User-Agent": UA,
