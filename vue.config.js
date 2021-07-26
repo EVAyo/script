54fe5807b365ba60d8a4175a846e79b528d2b13a
rosmontis233 <459595651@qq.com>
2021-07-26 18:19:55 +0800

    <feat> 增加了对抖音的反向代理

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   devServer: {
-    https:false,
     open:false,
     proxy:{
+      '/douyin':{
+        target:'https://www.iesdouyin.com',
+        secure:true,
+        changeOrigin:true,
+        pathRewrite:{
+          '^/douyin':'/'
+        }
+      },
       '/api':{
         target:'http://api.bilibili.com',
         changeOrigin:true,
