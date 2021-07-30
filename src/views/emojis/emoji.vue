30720e17f8fc128237f5f4dd1afe51ac3e750b94
锋子 <linker_fen@163.com>
2021-07-30 17:45:20 +0800

    <Fix>表情包页面窗口图片列数随宽度改变；

--- src/views/emojis/emoji.vue
+++ src/views/emojis/emoji.vue
@@ -2,8 +2,11 @@
   <div class="emoji">
 		<div class="waterfall" ref="waterfallBox">
       <div v-for="(bigItem,index) in imgList" :key="index" class="column">
-        <div v-for="img in bigItem" :key="img.id" class="column-item"  :style="{'padding-top':img.paddingTop+'%'}">
-				  <img :src="img.url" alt="" class="column-item-img" >
+        <div v-for="img in bigItem" :key="img.id" class="column-item fadeInUp"  :style="{'padding-top':img.paddingTop+'%'}">
+				  
+          <a :href="img.url" target="_blank" rel="noopener noreferrer">
+              <img :src="img.url" alt="" class="column-item-img" >
+          </a>
 			  </div>
       </div>
 		</div>
@@ -16,30 +19,14 @@ export default {
 
   data() {
     return {
-      imgList: [
-        [{
-          paddingTop:"",
-          width:"",
-          id:"1"
-        }],
-        [{
-          paddingTop:"",
-          width:"",
-          id:"0"
-        }],[{
-          paddingTop:"",
-          width:"",
-          id:"2"
-        }],
-      ],
-      // imgList: Array.from(Array(this.columnNum), () => new Array(0)),
+      // 当前获取的所有数据的一维数组
+      cacheList:[],
+      imgList: [],
       // 当前页
       currentPage:1,
       count: 0,
-      // 瀑布盒子高度
-      boxHeight:0,
       screenWidth: document.body.clientWidth,
-      columnNum:4,
+      columnNum:3,
     };
   },
   created() {
@@ -50,27 +37,24 @@ export default {
           this.listensBoxScroll()
           // debugger
             window.onresize = () => {
-                // return (() => {
                     window.screenWidth = document.body.clientWidth
                     this.screenWidth = window.screenWidth
-                    // console.log(this.screenWidth);
-                // })()
             }
   },
   watch:{
     
     screenWidth (val) {
-                 if (!this.timer) {
+                if (!this.timer) {
                     this.screenWidth = val
                     this.timer = true
                     // let that = this
                     setTimeout(()=> {
-                      let tempColumn = Math.floor((this.screenWidth*0.8)/320)
-                      // if(this.columnNum!=tempColumn){
-                      //     this.columnNum = tempColumn;
-                      //     this.setListIndex()
-                      // }
-                       
+                      let tempColumn = Math.floor((this.screenWidth*0.8)/340)
+                      if(this.columnNum!=tempColumn){
+                          this.columnNum = tempColumn;
+                          this.setListIndex()
+                      }
+                      
                     // console.log(      );  
                         // that.screenWidth = that.$store.state.canvasWidth
                         console.log(this.screenWidth,'this.screenWidth')
@@ -82,42 +66,29 @@ export default {
   },
  
   methods: {
-    // 设置数组列数
+    // 重置展示数组列数
     setListIndex(){
-      return 
-      const imgList = [...this.imgList]
-      // [[],[],[],[]]
-      // [[][][][][]]  row =  2
-      // [[][][][][]]  male = 5
-      let List = []
-      let index = 0;
-      let tempList = Array.from(Array(this.columnNum),() => new Array())
+      const cacheList = [...this.cacheList]
+      const column  = this.columnNum
+      let tempList = Array.from(Array(column),() => new Array())
       // this.imgList 
-      let maxLength = imgList[0].length
-      let Row = imgList.length
-      for(let i=0;i<maxLength;i++){
-        for(let row = 0;row<Row;row++){
-          if(!imgList[i][row]){
-            break
-          }
-          index++
-          tempList[index%this.columnNum].push(imgList[i][row])
-        }
-      }
+      cacheList.forEach((ele,index)=>{
+          let temp = index%this.column
+          tempList[temp].push(ele)
+      })
       this.imgList = tempList
-      // console.log(tempList);
-      // debugger
     },
     // 监听waterfallBox的scroll事件
     listensBoxScroll(){
       this.$refs.waterfallBox.onscroll  = (e) => {
-           if (!this.timeBoxScroll) {
+          if (!this.timeBoxScroll) {
                     this.timeBoxScroll = true
                     setTimeout(async()=> {
-                      // 触底
-                      if(this.$refs.waterfallBox.scrollTop
-                      +this.$refs.waterfallBox.offsetHeight
-                      >=this.$refs.waterfallBox.scrollHeight ){
+                      // 目前窗口底部离容器顶部的距离  
+                      let  TopOffsetHeight = this.$refs.waterfallBox.scrollTop +this.$refs.waterfallBox.offsetHeight
+                      let scrollHeight  = this.$refs.waterfallBox.scrollHeight 
+                      // 离底部50px触发翻页
+                      if(TopOffsetHeight +50 >= scrollHeight){
                         this.currentPage++;
                         await this.GetLIstImg()
                       }
@@ -126,35 +97,28 @@ export default {
                 }
       }
     },
-    async GetLIstImg(pageSize=10) {
+    async GetLIstImg(pageSize=20) {
       try {
         this.$loading();
-        const res = await this.$request({
-          url: `http://124.156.217.253:8000/?page=${this.currentPage}&limit=${pageSize}`,
-        });
         // const res = await this.$request({
-        //   url: `emoji/?page=${this.currentPage}&limit=${pageSize}`,
+        //   url: `http://124.156.217.253:8000/?page=${this.currentPage}&limit=${pageSize}`,
         // });
+        const res = await this.$request({
+          url: `emoji/?page=${this.currentPage}&limit=${pageSize}`,
+        });
 
         let tempList =  [...this.imgList]
-        // console.log(tempList);
-        // debugger
+
         res.forEach((ele,index)=>{
-            let i = (index+ ((this.currentPage-1)*10))%this.columnNum
-            console.log(i);
+            let i = (index+ ((this.currentPage-1)*20))%this.columnNum
             ele.paddingTop=ele.height/ ele.width *100
-            // let temp = Math.floor(Math.random()*100*2)
-            // ele.paddingTop= temp>20&&temp<160 ? temp : '100'
             ele.url = 'https://'+ ele.url
             tempList[i].push(ele)
-            // this.imgList[i].push(ele)
-            // tempList.push({
-            //   id:ele.id,
-            //   url: 'https://'+ ele.url
-            // })
+            this.cacheList.push(ele)
         })
         this.imgList = [...tempList]  
       } catch (error) {
+        this.$message({message:error})
         console.log(error);
       } finally {
         //console.log(res);
@@ -169,9 +133,6 @@ export default {
 
 
 <style lang="less" scoped>
-
-
-
 .emoji{
   position: relative;
   width: 100%;
@@ -179,34 +140,55 @@ export default {
   background-image:url('../../assets/img/emoji/bgp.png');
   background-size: cover;
   background-color: #000;
-   display: flex;
+  display: flex;
   justify-content: center;
 }
 
 .waterfall{
   position: absolute;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.8);
   display: flex;
+  flex-wrap: wrap;
   width: 80%;
-  min-width: 700px;
+  min-width: 340px;
   justify-content: center;
   height: 100vh;
   overflow-y: auto;
 }
 .column{
-  width: 300px;
+  max-width: 300px ;
+  width: 100%;
+  min-width: 100px;
   // background-color: blue;
-  margin: 0 10px;
+  margin: 0 20px;
 
 }
 .column-item {
+  display: block;
   position: relative;
   width: 100%;
   height: 0;
   // height: 50px;
   // padding: 50%;
-  margin: 10px 0 ;
+  margin: 20px 0 ;
   background-color: rgba(255, 255, 255);
+  // border-radius: 4px;
+  // transition: transform 0.5s ease-in;
+  // transition: height 0.5s ;
+  transform: scale(0.95,0.95);
+  transition: transform 0.3s ease-out,background-color 0.3s ease-out;
+}
+// .ftco-animate {
+//     opacity: 0;
+//     visibility: hidden;
+// }
+
+.column-item:hover {
+  transform: scale(1.05,1.05);
+  background-color: rgba(255, 255, 255,0);
+  // margin: 20px;
+  // width: 110%;
+  // height: auto;
 }
 .column-item-img{
     position: absolute;
@@ -214,16 +196,6 @@ export default {
     left: 0;
     height: 100%;
     width: 100%;
-    
+  // border-radius: 4px;   
 }
-//  img {
-//   position: absolute;
-//   /* 主要 */
-//   /* width: auto;height: auto; */
-//   // width: 90%;
-//   // height: auto;
-//   /* max-height: 100%; */
-//   /* 次要 */
-//   border-radius: 6px;
-// }
 </style>
