d152ddfe28c5c859e3f5f395801c77666fbe2746
maggot_code <wowhuanhuan233@163.com>
2021-08-03 12:45:06 +0800

    <fix> proxy update
    1. 修改bilibili api 代理
    2. 修改douyin api 代理
    3. 增加成分姬 api 代理

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,27 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2021-08-02 22:21:50
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-08-03 12:43:53
+ * @Description: file content
+ */
 module.exports = {
   devServer: {
     open: false,
     proxy: {
-      '/douyin': {
+      '/api/px/douyin': {
         target: 'https://www.iesdouyin.com',
         secure: true,
         changeOrigin: true,
         pathRewrite: {
-          '^/douyin': '/'
+          '^/api/px/douyin': '/'
         }
       },
-      '/bilibili': {
+      '/api/px/bilibili': {
         target: 'http://api.bilibili.com',
         changeOrigin: true,
         pathRewrite: {
-          '^/bilibili': '/'
+          '^/api/px/bilibili': '/'
         }
       },
       '/emoji': {
@@ -22,6 +29,13 @@ module.exports = {
         changeOrigin: true,
         pathRewrite: {
           '^/emoji': '/'
+        }
+      },
+      '/api/cfj': {
+        target: 'http://ilovemiku.cn:7123',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/cfj': '/cfj'
         }
       },
       '/api': {
