24c98e851dc32a0c5511ae0753cdfc9977f365f7
ekoMax <ekoMax@gmail.com>
2021-08-04 04:24:30 +0800

    迁移过期活动

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -12,8 +12,6 @@
 21 0,12,22 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 #燃动夏季下注 活动时间：2021-07-8到2021-08-8
 11 12,18,20 * * * node /scripts/jd_summer_movement_bet.js >> /scripts/logs/jd_summer_movement_bet.log 2>&1
-#女装盲盒 活动时间：2021-05-24到2021-06-22
-35 1,22 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 #京东极速版红包(活动时间：2021-5-5至2021-5-31)
 45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 #超级直播间红包雨(活动时间不定期，出现异常提示请忽略。红包雨期间会正常)
