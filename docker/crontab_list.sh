619059ed35494dbb0bd3070bf1278ce7e0059463
ekoMax <ekoMax@gmail.com>
2021-08-13 20:06:18 +0800

    骁龙品牌日

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,7 +5,7 @@
 
 ##############短期活动##############
 #骁龙品牌日 2021年08月13日-2021年08月25日
-12 7,22 * * * * * * node /scripts/jd_xl.js >> /scripts/logs/jd_xl.log 2>&1
+12 1,22 * * * * * * node /scripts/jd_xl.js >> /scripts/logs/jd_xl.log 2>&1
 
 #金机奖投票 2021年08月8日-2021年08月20日
 39 1,7,20 * * * node /scripts/jd_golden_machine.js >> /scripts/logs/jd_golden_machine.log 2>&1
