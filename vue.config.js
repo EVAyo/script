fb295f81bb0c98e6849e7dd31a4c21e054efaaed
锋子 <linker_fen@163.com>
2021-08-16 11:21:15 +0800

    <Fix>更换API配置

--- vue.config.js
+++ vue.config.js
@@ -25,21 +25,21 @@ module.exports = {
         }
       },
       '/emoji': {
-        target: 'https://meme-api.asoulfan.cn/',
+        target: 'https://meme-api.asoulfan.com/',
         changeOrigin: true,
         pathRewrite: {
           '^/emoji': '/'
         }
       },
       '/api/cfj': {
-        target: 'http://ilovemiku.cn:7123',
+        target: 'http://ilovemiku.com:7123',
         changeOrigin: true,
         pathRewrite: {
           '^/api/cfj': '/cfj'
         }
       },
       '/api': {
-        target: 'https://support-api.asoulfan.cn/api',
+        target: 'https://support-api.asoulfan.com/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/'
