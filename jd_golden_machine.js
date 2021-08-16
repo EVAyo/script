f13b6e24c5972bfa6faddc6f4a2c40291387942e
FateLight <fatelightly@gmail.com>
2021-08-16 11:35:29 +0000

    update jd_golden_machine.js.

--- jd_golden_machine.js
+++ jd_golden_machine.js
@@ -26,7 +26,7 @@ let res = [];
         return;
     }
 
-    try{res = await getAuthorShareCode('http://qwde58xzt.hn-bkt.clouddn.com/goldPhone.json');}catch (e) {}
+    try{res = await getAuthorShareCode('http://qxxkzb4qq.hn-bkt.clouddn.com/goldPhone.json');}catch (e) {}
     if(res && res.length > 0){
         $.authorCode = getRandomArrayElements(res,1)[0];
     }
