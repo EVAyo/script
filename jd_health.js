4a445e1e11fde2f4a72a94b0ef34ae7cffa2686b
eee <eeeee@io.com>
2021-10-15 19:30:07 +0800

    Auto sync scripts

--- jd_health.js
+++ jd_health.js
@@ -324,7 +324,7 @@ function safeGet(data) {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `https://gitee.com/dockere/jd-base/raw/master/null.json
+    $.get({url: `https://gitee.com/dockere/jd-base/raw/master/null.json, timeout: 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(JSON.stringify(err))
