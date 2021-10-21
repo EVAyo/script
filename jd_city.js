5d2c732f868064a8d48186b2afde32ef99591f5c
ekoMax <ekoMax@gmail.com>
2021-10-22 00:44:38 +0800

    update

--- jd_city.js
+++ jd_city.js
@@ -36,8 +36,8 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 let inviteCodes = [
-  'RtGKrqTCF0nZAcXiYIBGmjxMiR8vxpKs-bwllKwxMNC-iSn4@RtGKrqzlBnLQFtHCQIxKmoW_c-R-23AJJn10swEVyoArcFzr',
-  'RtGKrqTCF0nZAcXiYIBGmjxMiR8vxpKs-bwllKwxMNC-iSn4@RtGKrqzlBnLQFtHCQIxKmoW_c-R-23AJJn10swEVyoArcFzr']
+  'Werjyr-mRAKkLNKdQd1lmmQSTXeJjakKnY902d17UakHHM49@RtGKzO2hSQ33LNbJFNwx1E-Q1tDn9K8F8qFofiSm-kb9IkmSbg',
+  'Werjyr-mRAKkLNKdQd1lmmQSTXeJjakKnY902d17UakHHM49@RtGKzO2hSQ33LNbJFNwx1E-Q1tDn9K8F8qFofiSm-kb9IkmSbg']
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
