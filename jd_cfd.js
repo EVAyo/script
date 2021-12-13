9afd2ebdf95e292287478ac9601b27e9fb762abd
eee <eeeee@io.com>
2021-12-13 20:00:53 +0800

    Auto sync av scripts

--- jd_cfd.js
+++ jd_cfd.js
@@ -1712,7 +1712,7 @@ async function requestAlgo() {
       "expandParams": ""
     })
   }
-  new Promise(async resolve => {
+  return new Promise(async resolve => {
     $.post(options, (err, resp, data) => {
       try {
         if (err) {
