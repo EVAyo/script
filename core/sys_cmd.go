fdecd67b5b0a39133dd05ae032b3d1ea183acb78
cdle <cdle@apple.com>
2022-08-06 21:14:30 +0800

    update

--- core/sys_cmd.go
+++ core/sys_cmd.go
@@ -159,7 +159,6 @@ func initSys() {
 						pxs = append(pxs, p)
 					}
 					pxs = append(pxs, "")
-					pxs = append(pxs, "https://gitee.yanyuge.workers.dev/")
 					pxs = append(pxs, "https://ghproxy.com/")
 					for _, prefix := range pxs {
 						if str == "" && s.GetImType() != "fake" {
